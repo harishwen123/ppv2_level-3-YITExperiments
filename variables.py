e0d9450533909d4e8b795f4ef893d2de91589dab
MATHUSHAN2010 <134823341+MATHUSHAN2010@users.noreply.github.com>
2023-05-30 11:34:19 +0530

    my projects

---
+++ variables.py
@@ -0,0 +1,29 @@
+3 4.12.ob1 (tags/v3.12.ob1:5612078, May 22 2023, 16:20:31) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=12
+>>> print(age)
+12
+>>> age=54
+>>> print(age)
+54
+>>> weight=41
+>>> print(age)
+54
+>>> print(weight)
+41
+>>> HEISHGT
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    HEISHGT
+NameError: name 'HEISHGT' is not defined
+>>> 
+>>> 
+
+>>> Height=169
+>>> print(heigh)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    print(heigh)
+NameError: name 'heigh' is not defined. Did you mean: 'Height'?
+>>> print(Height)
+169
